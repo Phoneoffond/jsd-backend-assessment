3a3dd89abfcc65fe435cfae1e5c13e76dd18bbfe
fon.suteara <fon.suteara@gmail.com>
2026-09-11 14:20:58 +0700

    backend

---
+++ server/src/productDB/products.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    id: "1",
+    name: "Monalisa",
+    price: 30000000,
+    quantity: 1
+  },
+  {
+    id: "2",
+    name: "Last Supper",
+    price: 27000000,
+    quantity: 1
+  },
+  {
+    id: "3",
+    name: "The Virgin of the Rocks",
+    price: 25000000,
+    quantity: 1
+  },
+  {
+    id: "4",
+    name: "The Creation of Adam",
+    price: 23000000,
+    quantity: 1
+  },
+  {
+    id: "5",
+    name: "School of Athens",
+    price: 20000000,
+    quantity: 1
+  },
+  {
+    id: "6",
+    name: "Venus of Urbino by Titian",
+    price: 19000000,
+    quantity: 1
+  },
+  {
+    id: "7",
+    name: "The Virgin, the Child Jesus, and St. Anne",
+    price: 17500000,
+    quantity: 1
+  },
+  {
+    id: "8",
+    name: "Vitruvian Man",
+    price: 15500000,
+    quantity: 1
+  },
+  {
+    id: "9",
+    name: "The fetus in the womb",
+    price: 13500000,
+    quantity: 1
+  },
+  {
+    id: "10",
+    name: "Lady with an Ermine",
+    price: 11750000,
+    quantity: 1
+  }
+];
---
+++ server/src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mainRouter from "./routes/index.js";
+import v1Router from "./routes/v1/index.js";
+import cors from "cors";
+
+const app = express();
+
+async function start() {
+  try {
+   
+    await app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to the database:", err.message);
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+app.use(express.json());
+app.use("/", mainRouter);
+app.use("/api/v1", v1Router)
+
+app.use(cors()); // เปิด CORS หาก React อยู่คนละ port เพื่อเชื่มกันได้
+app.use(express.json());
+
+// Centralize error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    error: "Something went wrong on the server",
+  });
+});
+
+const port = 5174;
+
+start();
